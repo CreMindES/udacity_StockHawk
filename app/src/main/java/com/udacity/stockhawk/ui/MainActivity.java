708febdcb889d267d329f23ddad538b125594ab0
cremindes <cremindes@gmail.com>
2017-05-01 20:26:59 +0200

    Added missing content descriptions.

--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -161,8 +161,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         if (PrefUtils.getDisplayMode(this)
                 .equals(getString(R.string.pref_display_mode_absolute_key))) {
             item.setIcon(R.drawable.ic_percentage);
+            // setTitle internally also calls setContentDescription
+            item.setTitle( "Percent" );
         } else {
             item.setIcon(R.drawable.ic_dollar);
+            // setTitle internally also calls setContentDescription
+            item.setTitle( "Dollar" );
         }
     }
 
